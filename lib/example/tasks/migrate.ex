59001c1e1aaa6dde148bebb40473150b00ca28f5
Nikhil Tilwalli <ntilwalli@gmail.com>
2018-10-01 17:40:59 -0400

    Minor changes to test auto-deployment

--- lib/example/tasks/migrate.ex
+++ lib/example/tasks/migrate.ex
@@ -5,6 +5,7 @@ defmodule Example.Tasks.Migrate do
     # Configure
     Mix.Releases.Config.Providers.Elixir.init(["${RELEASE_ROOT_DIR}/etc/config.exs"])
     repo_config = Application.get_env(:distillery_example, Example.Repo)
+    IO.inspect {:repo_config, repo_config}
     repo_config = Keyword.put(repo_config, :adapter, Ecto.Adapters.Postgres)
     Application.put_env(:distillery_example, Example.Repo, repo_config)
 
